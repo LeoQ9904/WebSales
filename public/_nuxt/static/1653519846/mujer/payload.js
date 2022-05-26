bba9938e4b0e7408692f0001350d15637db7ede3
Leonardo Quintero <qqjq9904@gmail.com>
2022-05-25 22:44:41 -0500

    Actualización de font y v-container

---
+++ public/_nuxt/static/1653519846/mujer/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/mujer", {data:[{}],fetch:{},mutations:[]});
---
+++ public/_nuxt/static/1653519846/mujer/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1653519846",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002Fmujer",config:{baseUrl:"https:\u002F\u002Fwebsalesleonardo.herokuapp.com\u002Fapi\u002F",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
